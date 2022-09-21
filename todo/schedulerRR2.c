0854b96584deb9f26260fcd9940c829adc87298a
Gustavo Sotres <gus.sotres@gmail.com>
2022-09-20 20:31:22 -0500

    Round Robin q=2

--- todo/schedulerRR2.c
+++ todo/schedulerRR2.c
@@ -8,51 +8,63 @@ extern int unblockevent;
 QUEUE ready;
 QUEUE waitinginevent[MAXTHREAD];
 
+int q_count = 0;
+
 void scheduler(int arguments)
 {
-	int old,next;
-	int changethread=0;
-	int waitingthread=0;
-	
-	int event=arguments & 0xFF00;
-	int callingthread=arguments & 0xFF;
+	int old, next;
+	int changethread = 0;
+	int waitingthread = 0;
 
-	if(event==NEWTHREAD)
+	int event = arguments & 0xFF00;
+	int callingthread = arguments & 0xFF;
+
+	if (event == TIMER)
+	{
+		q_count++;
+		if (q_count == 2)
+		{
+			threads[callingthread].status = READY;
+			_enqueue(&ready, callingthread);
+			changethread = 1;
+			q_count = 0;
+		}
+	}
+
+	if (event == NEWTHREAD)
 	{
 		// Un nuevo hilo va a la cola de listos
-		threads[callingthread].status=READY;
-		_enqueue(&ready,callingthread);
+		threads[callingthread].status = READY;
+		_enqueue(&ready, callingthread);
 	}
-	
-	if(event==BLOCKTHREAD)
+
+	if (event == BLOCKTHREAD)
 	{
 
-		threads[callingthread].status=BLOCKED;
-		_enqueue(&waitinginevent[blockevent],callingthread);
+		threads[callingthread].status = BLOCKED;
+		_enqueue(&waitinginevent[blockevent], callingthread);
 
-		changethread=1;
+		changethread = 1;
 	}
 
-	if(event==ENDTHREAD)
+	if (event == ENDTHREAD)
 	{
-		threads[callingthread].status=END;
-		changethread=1;
-	}
-	
-	if(event==UNBLOCKTHREAD)
-	{
-			threads[callingthread].status=READY;
-			_enqueue(&ready,callingthread);
+		threads[callingthread].status = END;
+		changethread = 1;
 	}
 
-	
-	if(changethread)
+	if (event == UNBLOCKTHREAD)
 	{
-		old=currthread;
-		next=_dequeue(&ready);
-		
-		threads[next].status=RUNNING;
-		_swapthreads(old,next);
+		threads[callingthread].status = READY;
+		_enqueue(&ready, callingthread);
 	}
 
+	if (changethread)
+	{
+		old = currthread;
+		next = _dequeue(&ready);
+
+		threads[next].status = RUNNING;
+		_swapthreads(old, next);
+	}
 }
